bb25300e102fb20408311381380d22c38fb90f36
KID <jyhsu2000@yahoo.com.tw>
2018-11-21 02:18:22 +0800

    Prevent to trigger physics event on chunk border Prevent lag

--- src/me/daddychurchill/CityWorld/Support/SupportBlocks.java
+++ src/me/daddychurchill/CityWorld/Support/SupportBlocks.java
@@ -68,6 +68,12 @@ public abstract class SupportBlocks extends AbstractBlocks {
     }
 
     public final void setBlockWithPhysics(int x, int y, int z, Material material) {
+        // Prevent to trigger physics event on chunk border
+        if (x == 0 || x == 15 || z == 0 || z == 15) {
+//            Bukkit.getServer().broadcastMessage("Border physics: " + this.sectionX + ", " + this.sectionZ + "(" + x + ", " + y + ", " + z + ")");
+            setBlock(x, y, z, material);
+            return;
+        }
         boolean was = setDoPhysics(true);
         setBlock(x, y, z, material);
         setDoPhysics(was);
@@ -145,6 +151,12 @@ public abstract class SupportBlocks extends AbstractBlocks {
     }
 
     public final void setBlocksWithPhysics(int x, int y1, int y2, int z, Material material) {
+        // Prevent to trigger physics event on chunk border
+        if (x == 0 || x == 15 || z == 0 || z == 15) {
+//            Bukkit.getServer().broadcastMessage("Border physics: " + this.sectionX + ", " + this.sectionZ + "(" + x + ", " + y1 + ", " + z + ")");
+            setBlocks(x, y1, y2, z, material);
+            return;
+        }
         boolean was = setDoPhysics(true);
         setBlocks(x, y1, y2, z, material);
         setDoPhysics(was);
@@ -163,6 +175,12 @@ public abstract class SupportBlocks extends AbstractBlocks {
     }
 
     public final void setBlocksWithPhysics(int x1, int x2, int y1, int y2, int z1, int z2, Material material) {
+        // Prevent to trigger physics event on chunk border
+        if (x1 == 0 || x2 == 16 || z1 == 0 || z2 == 16) {
+//            Bukkit.getServer().broadcastMessage("Border physics: " + this.sectionX + ", " + this.sectionZ + "(" + x1 + ", " + y1 + ", " + z1 + ")");
+            setBlocks(x1, x2, y1, y2, z1, z2, material);
+            return;
+        }
         boolean was = setDoPhysics(true);
         for (int x = x1; x < x2; x++) {
             for (int y = y1; y < y2; y++) {
@@ -185,12 +203,24 @@ public abstract class SupportBlocks extends AbstractBlocks {
     }
 
     public final void setBlocksWithPhysics(int x1, int x2, int y, int z1, int z2, Material material) {
+        // Prevent to trigger physics event on chunk border
+        if (x1 == 0 || x2 == 16 || z1 == 0 || z2 == 16) {
+//            Bukkit.getServer().broadcastMessage("Border physics: " + this.sectionX + ", " + this.sectionZ + "(" + x1 + ", " + y + ", " + z1 + ")");
+            setBlocks(x1, x2, y, z1, z2, material);
+            return;
+        }
         boolean was = setDoPhysics(true);
         setBlocks(x1, x2, y, z1, z2, material);
         setDoPhysics(was);
     }
 
     public final void setBlocksUpwardWithPhysics(int x, int y1, int z, int maxY, Material material) {
+        // Prevent to trigger physics event on chunk border
+        if (x == 0 || x == 15 || z == 0 || z == 15) {
+//            Bukkit.getServer().broadcastMessage("Border physics: " + this.sectionX + ", " + this.sectionZ + "(" + x + ", " + y1 + ", " + z + ")");
+            setBlocksUpward(x, y1, z, maxY, material);
+            return;
+        }
         boolean was = setDoPhysics(true);
         setBlocksUpward(x, y1, z, maxY, material);
         setDoPhysics(was);
